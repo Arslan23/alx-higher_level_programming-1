647b0286d28defdd450a7a08dea002b03643a6a5
daiz93 <desmond.kpo@gmail.com>
2022-06-03 08:38:01 +0100

    nfinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    import re
+    i = 0
+    arg_len = 0
+    type_c = 0
+    r = ""
+    _sum = 0
+
+    argv_count = len(sys.argv) - 1
+    if argv_count > 0:
+        for i in range(argv_count):
+            arg_len = len(sys.argv[i + 1])
+            for j in range(arg_len):
+                r = sys.argv[i + 1]
+                if r.find('[0-9]*') is None:
+                    type_c = 1
+            if type_c != 1:
+                _sum = _sum + int(sys.argv[i + 1])
+                type_c = 0
+    print("{}".format(_sum))
