b0c03fe28a29510a5ed950b19636af1c7ad76d9d
daiz93 <desmond.kpo@gmail.com>
2022-06-04 05:44:19 +0100

    remove unsued variable

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    i = 0
     argv_count = len(sys.argv) - 1
     if argv_count == 0:
         print("0 argument.")
@@ -11,4 +10,4 @@ if __name__ == "__main__":
         print("{} arguments:".format(argv_count))
     for i in range(argv_count):
         '''print("{}: ".format(i + 1) + "{}".format(sys.argv[i + 1]))'''
-        print ("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
