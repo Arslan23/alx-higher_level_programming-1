32ea1253847a61dff044487dda931b53477d8eca
daiz93 <desmond.kpo@gmail.com>
2022-06-04 05:56:01 +0100

    hOW to make a script dynamaic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
+
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argv_count = len(sys.argv)
-    if argv_count == 1:
-        print("{} arguments.".format(argv_count - 1))
-    elif argv_count == 2:
-        print("{} argument:".format(argv_count - 1))
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(argv_count - 1))
-    for i in range(1, argv_count):
-        print("{}: {}".format(i + 1, sys.argv[i]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
