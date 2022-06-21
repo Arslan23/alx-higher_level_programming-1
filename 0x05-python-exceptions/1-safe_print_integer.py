656593ffbb61d5aa2ac5410036274657f096f44f
daiz93 <desmond.kpo@gmail.com>
2022-06-21 05:37:49 +0100

    use finally

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 
 def sdef safe_print_integer(value):
     try:
-        print('{:d}'.format(value), end="")
+        print("{:d}".format(value), end="")
         return true
-    except ValueError:
+    finally
         return false
