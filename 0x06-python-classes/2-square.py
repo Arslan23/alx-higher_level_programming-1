55eb0a56895de5db11b1949b8ea1d11af196c413
daiz93 <desmond.kpo@gmail.com>
2022-06-22 07:04:25 +0100

    Another way

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,15 @@
 
 #!/usr/bin/python3
-"""Defines a class Square."""
-
-
 class Square:
-    """Represents a square."""
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with optional size.
+    """
 
     def __init__(self, size=0):
-        """Initialize a new Square.
-        Args:
-            size (int): The size of the new square.
-        """
+        """Initializes the data."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = 
+        self.__size = size
