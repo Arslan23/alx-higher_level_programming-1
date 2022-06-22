d88dd82d1fc38d3c71825e133533254375df287b
daiz93 <desmond.kpo@gmail.com>
2022-06-22 06:53:27 +0100

    init size upper than 0

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
+'''defines a square'''
+
 
 class Square:
-    def __init__(self,size = 0):
+    '''Represent a square'''
+    def __init__(self, size=0):
+        '''Init new Square
+
+        Args: size:
+
+            size(int) : Size of the new Square
+            size must be an integer upper than 0
+        '''
         try:
-            self.size = size
-        except Exception:
-            raise NameError("size must be an integer")
+        if (not isinstance(size, int)):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError ('size must be >= 0')
         else:
-            if (size <= 0):
-                raise NameError("size must be >= 0")
+        self.__size = size
+        
