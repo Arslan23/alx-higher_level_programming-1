b1207a12cc245576f35107259730170cd9a2d174
daiz93 <desmond.kpo@gmail.com>
2022-06-28 06:39:13 +0100

    Eval is magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,47 +1,47 @@
 #!/usr/bin/python3
-'''An empty class Rectangle that defines a rectangle'''
+"""Module 0-rectangle
+Defines an empty Rectangle class.
+"""
 
 
 class Rectangle:
-    '''Class tha represent a Rectangle'''
-    __init__(self, width=0, height=0):
-        ''' '''
-        self.__width = width
-        self.__height = height
+    """Represent a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        '''Get width'''
+        """Get/set the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        '''Set width'''
-        try:
-            if isinsert(value, int):
-                if value <= 0:
-                    raise ValueError("width must be >= 0")
-                else:
-                    self.__width = value
-            else:
-                raise TypeError("width must be an integer")
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
-        '''Get height'''
-        return self.__width
+        """Get/set the height of the rectangle."""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        '''Set height'''
-        try:
-            if isinsert(value, int):
-                if value <= 0:
-                    raise ValueError("height must be >= 0")
-                else:
-                    self.__height = value
-            else:
-                raise TypeError("height must be an integer")
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         '''Public methode area that return the rectangle area
@@ -73,7 +73,7 @@ class Rectangle:
 
     def __repr__(self):
         '''Methode print Rectangle with #
-    Return: representation'''
+    Return: (Rectangle) representation'''
         representation = Rectangle()
         if self.__height <= 0 or self.__width <= 0:
             representation = self
