8fa6357c03b41f2b36210c2bff8fc821291b45ef
daiz93 <desmond.kpo@gmail.com>
2022-08-12 19:04:16 +0100

    liste cities

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,3 @@
--- 
+-- lists all the cities of Californiag
+USE @database_name;
+SELECT CONCAT(`cities.id`, `cities.name`, `states.name`) FROM cities INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id
