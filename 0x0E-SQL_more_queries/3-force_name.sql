df315109649d194177b35b1e60fbce47bc736679
daiz93 <desmond.kpo@gmail.com>
2022-08-12 18:14:34 +0100

    Test bug

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,4 @@
 -- creates the table force_name
 USE @database_name;
+-- Create
 CREATE TABLE IF NOT EXISTS `force_name` (`id` INT, `name` VARCHAR(256) NOT NULL);
--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
--- reates the database hbtn_0d_usa
+-- Reates the database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (`id` INT PRIMARY KEY  NOT NULL AUTO_INCREMENTY, `name` VARCHAR(256) NOT NULL);
