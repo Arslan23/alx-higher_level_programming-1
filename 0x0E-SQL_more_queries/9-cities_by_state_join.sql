d16f592bcb96da251cac87fc5bb2c7062cb0d648
daiz93 <desmond.kpo@gmail.com>
2022-08-12 19:05:53 +0100

    review concat

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
--- lists all the cities of Californiag
+-- lists all cites
 USE @database_name;
-SELECT CONCAT(`cities.id`, `cities.name`, `states.name`) FROM cities INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id
+SELECT CONCAT(`cities.id`, '-',`cities.name`,'-', `states.name`) FROM cities INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id
