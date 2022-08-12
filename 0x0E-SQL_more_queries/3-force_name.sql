00e375ae59f34a58a7e570287954f3d69ad85473
daiz93 <desmond.kpo@gmail.com>
2022-08-12 06:04:01 +0100

    Write a script that c and

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 -- creates the table force_name
-USE @database_name
-CREATE TABLE IF NOT EXISTS force_name (`id` INT, `name` VARCHAR(256) NOT NULL)
+USE @database_name;
+CREATE TABLE IF NOT EXISTS force_name (`id` INT, `name` VARCHAR(256) NOT NULL);
--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- creates the table id_not_null 
-USE @database_name
-CREATE TABLE IF NOT EXISTS id_not_null (`id` INT NOT NULL DEFAULT 1, `name` VARCHAR(256))
+USE @database_name;
+CREATE TABLE IF NOT EXISTS id_not_null (`id` INT NOT NULL DEFAULT 1, `name` VARCHAR(256));
