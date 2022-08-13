1df9749e8a49e620e96d2d3d742f5d26ebf8077d
daiz93 <desmond.kpo@gmail.com>
2022-08-13 06:30:24 +0100

    cites reviez

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Create table states
-CREATE TABLE IF NOT EXISTS states (`id` INT UNIQUE  NOT NULL AUTO_INCREMENT, `name` VARCHAR(256) NOT NULL, PRIMARY KEY(`id`));
+CREATE TABLE IF NOT EXISTS states (`id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(256) NOT NULL, PRIMARY KEY(`id`));
 -- CREATE TABLE cities
-CREATE TABLE IF NOT EXISTS cities ('id' INT NOT NULL UNIQUE AUTO_INCREMENT, 'state_id' INT NOT NULL, 'name' VARCHAR(256), PRIMARY KEY ('id'), FOREIGN KEY ('state_id') REFERENCES states('id'));
+CREATE TABLE IF NOT EXISTS cities ('id' INT NOT NULL AUTO_INCREMENT, 'state_id' INT NOT NULL, 'name' VARCHAR(256) NOT NULL, PRIMARY KEY ('id'), FOREIGN KEY ('state_id') REFERENCES states('id'));
