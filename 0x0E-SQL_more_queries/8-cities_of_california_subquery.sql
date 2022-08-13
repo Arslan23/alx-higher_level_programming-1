a9eaf7fe514a7befc38e5745f615e06ba7162c2d
daiz93 <desmond.kpo@gmail.com>
2022-08-13 06:33:38 +0100

    delete use

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,2 @@
 -- lists all the cities of Californiag
-USE 
 SELECT cities.* FROM cities where cities.state_id IN ( SELECT states.id FROM states WHERE states.name = 'California' ) ORDER BY cities.id
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,2 @@
--- lists all cites
-USE 
+-- lists all cites 
 SELECT CONCAT(`cities.id`, '-',`cities.name`,'-', `states.name`) FROM cities INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id
