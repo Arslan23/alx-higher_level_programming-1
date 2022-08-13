15ab85e2e7ffd42d78ab7fdb529ac75f8728c5f9
daiz93 <desmond.kpo@gmail.com>
2022-08-13 06:42:30 +0100

    9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,3 @@
 -- lists all cites 
-SELECT CONCAT(`cities.id`, '-',`cities.name`,'-', `states.name`) FROM cities INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id
+SELECT `cities.id`,`cities.name`, `states.name`
+FROM cities INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id
