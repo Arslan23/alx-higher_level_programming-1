c66ae00219b85111587fd4759a0d3bdcc005ce6a
daiz93 <desmond.kpo@gmail.com>
2022-08-13 06:38:38 +0100

    delete use

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,4 @@
 -- lists all the cities of Californiag
-SELECT cities.* FROM cities where cities.state_id IN ( SELECT states.id FROM states WHERE states.name = 'California' ) ORDER BY cities.id
+SELECT `id`, `name` FROM `cities` where `cities.state_id` IN 
+( SELECT `states.id` FROM `states` WHERE `states.name` = 'California' ) 
+ORDER BY `cities.id`;
